905c3359939d935afdee358fac5b7d22373a3d2e
Stanley Wintergreen <kepocnhh@gmail.com>
2023-02-02 14:44:23 +0300

    benchmark sorted + sorted half + reversed + equivalents

--- lib/src/jmh/kotlin/test/jmh/sorting/SortingAlgorithmBenchmark.kt
+++ lib/src/jmh/kotlin/test/jmh/sorting/SortingAlgorithmBenchmark.kt
@@ -17,7 +17,7 @@ import kotlin.math.absoluteValue
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value = 1, warmups = 1)
+@Fork(value = 1, warmups = 0)
 @Measurement(iterations = 1, time = 1)
 @Warmup(iterations = 0)
 @State(Scope.Benchmark)
@@ -26,29 +26,80 @@ internal open class SortingAlgorithmBenchmark {
         private const val SALT = "e43f98c5-9923-4cd3-b441-c1e812cf6604"
     }
 
-    @Param(value = ["4096", "16384", "65536"])
+//    @Param(value = ["4096"])
+    @Param(value = ["16384"])
+//    @Param(value = ["4096", "16384", "65536"])
     var size: Int = 0
 
-    private var list = listOf<Int>()
+    private var listShuffled = listOf<Int>()
+    private var listSorted = listOf<Int>()
+    private var listSortedHalf = listOf<Int>()
+    private var listReversed = listOf<Int>()
+    private val listEquivalents = List(size) { 42 }
 
     @Setup(Level.Trial)
     fun eachTrial() {
         val bytes = SALT.toByteArray()
         val hashCode = SALT.hashCode()
-        list = List(size) { index ->
+        listShuffled = List(size) { index ->
             val number = hashCode * size + index + 13
             val byte = bytes[number.absoluteValue % bytes.size]
             byte * hashCode / size + 13 - index
         }
+        listSorted = listShuffled.sorted()
+        listSortedHalf = listShuffled.let { list ->
+            list.subList(0, list.size / 2) + list.subList(list.size / 2 + 1, list.size - 1).sorted()
+        }
+        listReversed = listSorted.reversed()
     }
 
     @Benchmark
-    fun sortByBubble() {
-        BubbleSort.sort(list)
+    fun sortShuffledByBubble() {
+        BubbleSort.sort(listShuffled)
     }
 
     @Benchmark
-    fun sortByBubbleEx() {
-        BubbleExSort.sort(list)
+    fun sortSortedByBubble() {
+        BubbleSort.sort(listSorted)
+    }
+
+    @Benchmark
+    fun sortSortedHalfByBubble() {
+        BubbleSort.sort(listSortedHalf)
+    }
+
+    @Benchmark
+    fun sortReversedByBubble() {
+        BubbleSort.sort(listReversed)
+    }
+
+    @Benchmark
+    fun sortEquivalentsByBubble() {
+        BubbleSort.sort(listEquivalents)
+    }
+
+    @Benchmark
+    fun sortShuffledByBubbleEx() {
+        BubbleExSort.sort(listShuffled)
+    }
+
+    @Benchmark
+    fun sortSortedByBubbleEx() {
+        BubbleExSort.sort(listSorted)
+    }
+
+    @Benchmark
+    fun sortSortedHalfByBubbleEx() {
+        BubbleExSort.sort(listSortedHalf)
+    }
+
+    @Benchmark
+    fun sortReversedByBubbleEx() {
+        BubbleExSort.sort(listReversed)
+    }
+
+    @Benchmark
+    fun sortEquivalentsByBubbleEx() {
+        BubbleExSort.sort(listEquivalents)
     }
 }
